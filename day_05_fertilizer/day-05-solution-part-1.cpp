0693ac469aa512d26af75411353075fd98de9dfa
Jakob Feldmann <j.feldmann@campus.tu-berlin.de>
2024-01-13 19:05:30 +0100

    feat: solution day 5 part one(not right yet)

---
+++ day_05_fertilizer/day-05-solution-part-1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+using namespace std;
+
+vector<double> GetNumbersInLine(string line)
+{
+    vector<double> containedNumbers;
+    int firstDigitIndex = -1;
+    int lastDigitIndex = -1;
+    for (int i = 0; i < line.length(); i++)
+    {
+        int firstAsciiValue = (int)line.c_str()[i];
+        if (57 >= firstAsciiValue && firstAsciiValue >= 48)
+        {
+            if (firstDigitIndex == -1)
+            {
+                firstDigitIndex = i;
+            }
+        }
+        else if (firstDigitIndex != -1)
+        {
+            lastDigitIndex = i - 1;
+        }
+        if(i == line.length() -1){
+            lastDigitIndex = i;
+        }
+        if (firstDigitIndex != -1 && lastDigitIndex != -1){
+            string numberString = line.substr(firstDigitIndex, lastDigitIndex - firstDigitIndex + 1);
+            containedNumbers.push_back(stod(numberString));
+            firstDigitIndex = -1;
+            lastDigitIndex = -1;
+        }
+    }
+    return containedNumbers;
+}
+
+// Also detects some special characters
+bool LineStartsWithLetter(string line){
+    int firstValue = (int)line.c_str()[0];
+    return firstValue >= 65 && firstValue <= 122;
+}
+
+bool LineStartsWithDigit(string line){
+    int firstValue = (int)line.c_str()[0];
+    return firstValue >= 48 && firstValue <= 57;
+}
+
+double ApplyMappingToNumber(double number, vector<double> mapping){
+    double sourceBound = mapping[1];
+    double destinationBound = mapping[0];
+    double range = mapping[2];
+    if(number < sourceBound){
+        return number;
+    }
+    if(number > sourceBound + range){
+        return number;
+    }
+    return (number - sourceBound) + destinationBound;
+}
+
+int main()
+{
+	ifstream infile("input.txt");
+	if (infile.is_open())
+	{
+		string line;
+        // Get the seed values in the first line
+        getline(infile, line);
+        vector<double> seedNumbers = GetNumbersInLine(line);
+        vector<double> locationNumbers = seedNumbers;
+        vector<double> intermediateNumbers = seedNumbers;
+        vector<double> currentMapping;
+        bool newMapping = false;
+        while(getline(infile,line)){
+           if(LineStartsWithLetter(line)){
+                newMapping = true;
+                locationNumbers = intermediateNumbers;
+                continue;
+            }
+            else{
+                newMapping = false;
+            }
+            if(LineStartsWithDigit(line)){
+                currentMapping = GetNumbersInLine(line);
+                for (int i = 0; i < intermediateNumbers.size(); i++)
+                {
+                    if(intermediateNumbers[i] != locationNumbers[i]){
+                        continue;
+                    }
+                    double mappedNumber = ApplyMappingToNumber(locationNumbers[i], currentMapping);
+                    if(mappedNumber != locationNumbers[i]){
+                        intermediateNumbers[i] = mappedNumber;
+                    }
+                }
+            }
+        }
+        locationNumbers = intermediateNumbers;
+
+        cout << *min_element(locationNumbers.begin(), locationNumbers.end()) << endl;
+		infile.close();
+	}
+}
